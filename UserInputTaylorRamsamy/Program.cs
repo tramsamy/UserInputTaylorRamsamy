ac38843181dae4069b3da93b05084f75b9d4b4b3
Taylor Ramsamy <tramsamy@usf.edu>
2020-01-30 22:33:56 -0500

    Add project files.

---
+++ UserInputTaylorRamsamy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UserInputTaylorRamsamy
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hi, this is Taylor Ramsamy!");
+			Console.WriteLine("Please type something. Then click the enter key.");
+			Console.ReadLine();
+			Console.WriteLine("Please type something else out. Then click the enter key.");
+			string TypedComment;
+			TypedComment = Console.ReadLine();
+			Console.WriteLine("The statement you have just typed is:" + TypedComment);
+
+		}
+	}
+}
